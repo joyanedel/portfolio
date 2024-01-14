467f631633bf7170db7e0ee45eb9ed8033a728bd
joyanedel <joyanede@alumnos.inf.utfsm.cl>
2024-01-14 12:08:20 -0300

    Fix navbar light bg

--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -20,7 +20,7 @@
 	}
 </script>
 
-<nav class="bg-white border-gray-200 dark:bg-gray-900 w-full justify-center">
+<nav class="border-gray-200 dark:bg-gray-900 w-full justify-center">
 	<div
 		class="max-w-screen-xl flex flex-wrap items-center justify-center gap-4 mx-auto p-4 w-full pt-[4rem] sm:pt-0"
 	>
@@ -52,13 +52,13 @@
 		</button>
 		<div class="hidden w-full md:block md:w-auto" id="navbar-default">
 			<ul
-				class="font-medium flex flex-col p-0 mt-4 border rounded-lg bg-gray-50 sm:flex-row sm:space-x-8 rtl:space-x-reverse sm:mt-0 sm:border-0 sm:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+				class="font-medium flex flex-col p-0 mt-4 rounded-lg bg-gray-50/20 sm:flex-row sm:space-x-8 rtl:space-x-reverse sm:mt-0 sm:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
 			>
 				<li>
 					<a
 						href="#cover"
 						on:click|preventDefault={clickAndScrollIntoView}
-						class="block py-2 px-3 text-white bg-blue-700 rounded sm:bg-transparent sm:text-blue-700 md:p-0 dark:text-white sm:dark:text-blue-500"
+						class="block py-2 px-3 text-slate-200 bg-blue-700 rounded sm:bg-transparent sm:text-blue-700 md:p-0 sm:dark:text-blue-500"
 						aria-current="page">Home</a
 					>
 				</li>
@@ -66,7 +66,7 @@
 					<a
 						href="#experience"
 						on:click|preventDefault={clickAndScrollIntoView}
-						class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+						class="block py-2 px-3 text-slate-200 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
 						>Experience</a
 					>
 				</li>
@@ -74,7 +74,7 @@
 					<a
 						href="#stack"
 						on:click|preventDefault={clickAndScrollIntoView}
-						class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+						class="block py-2 px-3 text-slate-200 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
 						>Stack</a
 					>
 				</li>
