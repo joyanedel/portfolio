14090cc8e1673b01658d66ccc1412f35f0150603
joyanedel <joyanede@alumnos.inf.utfsm.cl>
2024-09-04 21:58:03 -0400

    Change svelte for astro

--- src/lib/components/IconWrapper.svelte
+++
@@ -1,3 +0,0 @@
-<i class="fill-slate-500 flex flex-col items-center justify-center w-44 sm:w-full p-6">
-	<slot />
-</i>
--- src/lib/components/StackItem.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	export let title: string;
-</script>
-
-<li class="flex flex-col items-center justify-center">
-	<slot />
-	<strong class="dark:text-slate-200">{title}</strong>
-</li>
